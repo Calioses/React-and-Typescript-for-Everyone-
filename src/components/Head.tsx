50e9de66e1896626dc67253431f41d73baafdc1e
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-03-17 13:25:34 -0400

    completed 5th vide

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,9 +3,22 @@ import React from "react";
 // title
 // isActive
 
+type User = {
+    name: string
+}
+
 type Props = {
     title: string; //Required
     isActive?: boolean; //Optional
+    thing?: number;
+    thing2?: string[];
+    status?: "loading" | "loaded";
+    thing3?: {};
+    thing4?: {
+        name: string;
+    };
+    func?: () => void;
+    user?: User;
 };
 
 export const Head = ({ title, isActive = true }: Props) => {
